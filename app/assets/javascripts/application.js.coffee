0ecd3b610c29f49f545f7abc4ed95a792d112ee5
Daniel Davey <daniel@platform45.com>
2012-07-16 14:03:47 +0200

    AppDrone: myWinningTrade

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,13 @@
+//= require jquery
+//= require jquery_ujs
+//= require init
+//= require bootstrap-dropdown
+//= require bootstrap-tab
+//= require bootstrap-tooltip
+//= require bootstrap-popover
+//= require bootstrap-button
+//= require bootstrap-collapse
+//= require chosen-jquery
+//= require underscore
+//= require jquery.html5-placeholder-shim
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWinningTrade::Application.config.secret_token = '788ec2246f2511dd398d48a46987e3fe1b0e194f9b437a393d2aa52137bd4c7cbd9cbb69d4f5433aee16890b8d5859d0722ffcc305503adf833e93f5846ea819'
