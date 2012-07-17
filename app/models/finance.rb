bbd17c1af8ae7d96ee2144a8d6f116cbebf83f06
Daniel Davey <whoisdanieldavey@gmail.com>
2012-07-17 21:13:36 +0200

    [#31007539] Updated env to datatables for successful finance query.

--- app/models/finance.rb
+++ app/models/finance.rb
@@ -4,15 +4,25 @@ class Finance
 	cattr_accessor :credentials
 
 	def current_stock_details(symbol)
-		#execute_yql("select * from http://www.datatables.org/yahoo/finance/yahoo.finance.quotes where symbol='#{symbol}'")
-		execute_yql("show tables")
+		execute_yql("select * from yahoo.finance.quotes where symbol='#{symbol}'")
+		#execute_yql("show tables")
 	end
 
-	def execute_yql(query)
+	def execute_yql(yql_query)
 		consumer = OAuth::Consumer.new(self.credentials['consumer_key'], self.credentials['consumer_secret'], site: 'http://query.yahooapis.com')
 		access_token = OAuth::AccessToken.new(consumer)
 
-		response = access_token.request(:get, "/v1/yql?q=#{OAuth::Helper.escape(query)}&format=json")
-		JSON.parse(response.body)
+		params = {
+		  q: yql_query,
+		  env: 'http://datatables.org/alltables.env',
+		  format: 'json',
+		  diagnostics: false
+		}
+
+        # merge params into escaped query string
+		query_string = params.map{ |k,v| "#{k}=#{OAuth::Helper.escape(v)}" }.join('&')
+
+		response = access_token.request(:get, '/v1/yql?' + query_string)
+		return JSON.parse(response.body)
 	end
 end
