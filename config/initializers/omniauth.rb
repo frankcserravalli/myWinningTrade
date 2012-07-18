10481d2dc72d0bbf1a80635c320367766cded6f1
Daniel Davey <daniel@platform45.com>
2012-07-18 16:03:36 +0200

    Added omniauth strategies.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter, 'YcnQioKp32tmL0Od4zcXyA', 'ZpdqL3iBDiQ9l1eZ6Kjy8U42zdDxPLxWl32fyJNjfw'
+  provider :twitter, 'YcnQioKp32tmL0Od4zcXyA', 'ZpdqL3iBDiQ9l1eZ6Kjy8U42zdDxPLxWl32fyJNjfw' # daniel's account
+  provider :facebook, '106132422866392', '0685df32ec9873ef96c2cf8e2f622ceb' # pascal's account
+  provider :linkedin, 'e33gepa5m9nn', 'iNRTei2vCi8Mcd6L' # daniel's account
+  # provider :google_oauth2, '', '', {access_type: 'online', approval_prompt: ''}
 end
