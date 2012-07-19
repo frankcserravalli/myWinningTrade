f184b0aeee2417aca4ff2626e955c18480ca06a1
Daniel Davey <daniel@platform45.com>
2012-07-19 16:01:45 +0200

    [#31007539] Stock show page.

---
+++ app/controllers/stock_controller.rb
@@ -0,0 +1,5 @@
+class StockController < ApplicationController
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ MyWinningTrade::Application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
+  get '/stock/:symbol', to: 'stock#show', as: :stock, symbol: /\w{1,5}/
+
 end
