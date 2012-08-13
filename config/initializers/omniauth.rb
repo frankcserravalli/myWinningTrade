6b39fd682999e470ad360b724f80484674e6d23b
Frank C. Serravalli <frank@mywinningtrade.com>
2012-08-13 18:36:38 -0400

    Corrected the additional space included in the Facebook API.
    Good Catch Daniel

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :facebook, '	331752936918078', '	6dee4f074f905e98957e9328bf4d91a3' # Frank's account
+  provider :facebook, '331752936918078', '6dee4f074f905e98957e9328bf4d91a3' # Frank's account
   provider :linkedin, 'e33gepa5m9nn', 'iNRTei2vCi8Mcd6L' # daniel's account
   provider :openid, name: 'google', identifier: 'https://www.google.com/accounts/o8/id'
 end
