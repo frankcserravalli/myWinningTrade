3022e88cbb5572964b42c8397234676f5745ba18
Frank C. Serravalli <frank@mywinningtrade.com>
2012-08-13 12:41:34 -0400

    Changed the OpenID API for Facebook from Pascal's account to Frank's Account

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :facebook, '106132422866392', '0685df32ec9873ef96c2cf8e2f622ceb' # pascal's account
+  provider :facebook, '	331752936918078', '	6dee4f074f905e98957e9328bf4d91a3' # Frank's account
   provider :linkedin, 'e33gepa5m9nn', 'iNRTei2vCi8Mcd6L' # daniel's account
   provider :openid, name: 'google', identifier: 'https://www.google.com/accounts/o8/id'
 end
