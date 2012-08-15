678902213d0eefdcbf8ce6ecb8c87f2762f76521
Frank C. Serravalli <frank@mywinningtrade.com>
2012-08-15 12:45:03 -0400

    Changed LinkedIn API key to Frank's
    Yes Daniel, my key is better.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :facebook, '331752936918078', '6dee4f074f905e98957e9328bf4d91a3' # Frank's account
-  provider :linkedin, 'e33gepa5m9nn', 'iNRTei2vCi8Mcd6L' # daniel's account
+  provider :linkedin, 'xoc3a06gsosd', '41060V6v5K38dnV4' # Frank's Account is better.
   provider :openid, name: 'google', identifier: 'https://www.google.com/accounts/o8/id'
 end
