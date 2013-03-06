9067dccc7b806526bc1fcf60700e535ea792c6f1
Jason <jasontanner328@gmail.com>
2013-03-05 23:33:00 -0500

    re-added stripe tokens

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
 # Set your secret key: remember to change this to your live secret key in production
 
 Stripe.api_key = 'sk_live_DK6kT53UCpc5bESmDuih6SN8'
-STRIPE_PUBLISHABLE_KEY = 'sk_live_DK6kT53UCpc5bESmDuih6SN8'
+STRIPE_PUBLISHABLE_KEY = 'pk_live_TmKg4iN7j3xcbZlFIvIbooPi'
