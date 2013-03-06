3906e8b1a806714664290686e548861d611b8a8a
Jason <jasontanner328@gmail.com>
2013-03-05 23:43:33 -0500

    added test api keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
 # Set your secret key: remember to change this to your live secret key in production
 
-Stripe.api_key = 'sk_live_DK6kT53UCpc5bESmDuih6SN8'
-STRIPE_PUBLISHABLE_KEY = 'pk_live_TmKg4iN7j3xcbZlFIvIbooPi'
+Stripe.api_key = 'sk_test_kcF23wfgD1RtpC7BDFxPZWcs'
+STRIPE_PUBLISHABLE_KEY = 'pk_test_aT91zOBdU6ASLRE9xr3nFih2'
