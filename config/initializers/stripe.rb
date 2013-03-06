cbdd57417dce9250b55f63b180c8a6103c5c2640
Jason <jasontanner328@gmail.com>
2013-03-06 18:22:27 -0500

    added testing api for stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
 # Set your secret key: remember to change this to your live secret key in production
 
-Stripe.api_key = 'sk_live_DK6kT53UCpc5bESmDuih6SN8'
+Stripe.api_key = 'sk_test_kcF23wfgD1RtpC7BDFxPZWcs'
 STRIPE_PUBLISHABLE_KEY = 'pk_test_aT91zOBdU6ASLRE9xr3nFih2'
