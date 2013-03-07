04ad0d1528a59cc4e13e8bbbea04d8b473b9d4ca
Jason <jasontanner328@gmail.com>
2013-03-06 22:20:55 -0500

    added rack redirect in hopes of fixing a redirect issue

--- Gemfile
+++ Gemfile
@@ -143,3 +143,5 @@ gem "pdfkit", "~> 0.5.3"
 
 gem "wkhtmltopdf-binary", "~> 0.9.9.1"
 
+gem "rack-rewrite", "~> 1.3.3"
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,10 @@ MyWinningTrade::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+    r301 %r{.*}, 'https://www.mywinningtrade.com$&', :if => Proc.new {|rack_env|
+      rack_env['SERVER_NAME'] != 'www.mywinningtrade.com'
+    }
+  end
 end
