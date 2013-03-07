6f5efd9627143f6da19e3dd00508f33d115d0eb4
Jason <jasontanner328@gmail.com>
2013-03-07 06:09:39 -0500

    fixed adsense css

--- config/application.rb
+++ config/application.rb
@@ -64,6 +64,7 @@ module MyWinningTrade
 
     # This is used so that when we go through a third-party source such as posting on Facebook the third
     # party can return back to mywinningtrade.com
+=begin
     config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
       r301 %r{.*}, 'https://www.mywinningtrade.com$&', :if => Proc.new {|rack_env|
         rack_env['SERVER_NAME'] != 'www.mywinningtrade.com'
@@ -71,6 +72,8 @@ module MyWinningTrade
       r301 %r{.*}, 'https://www.mywinningtrade.com$&', :if => Proc.new {|rack_env|
         rack_env['SERVER_NAME'] == 'mywinningtrade.com'
       }
+
     end
+=end
   end
 end
