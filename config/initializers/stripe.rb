2067f25c12a3c3d4d71ccd0aa70bea1238ba0bdf
Jason <jasontanner328@gmail.com>
2013-03-11 19:27:35 -0400

    added stripe keys for testing, now rails automatically switches keys based on environment

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,11 @@
-# Set your secret key: remember to change this to your live secret key in production
+# Sets up the keys based on what environment Rails is in. Dev/Testing has the testing api keys.
 
-Stripe.api_key = 'sk_live_DK6kT53UCpc5bESmDuih6SN8'
-STRIPE_PUBLISHABLE_KEY = 'pk_live_TmKg4iN7j3xcbZlFIvIbooPi'
+if Rails.env == 'development' || Rails.env == 'staging' || Rails.env == 'test'
+  Stripe.api_key = 'sk_test_kcF23wfgD1RtpC7BDFxPZWcs'
+
+  STRIPE_PUBLISHABLE_KEY = 'pk_test_aT91zOBdU6ASLRE9xr3nFih2'
+elsif Rails.env == 'production'
+  Stripe.api_key = 'sk_live_DK6kT53UCpc5bESmDuih6SN8'
+
+  STRIPE_PUBLISHABLE_KEY = 'pk_live_TmKg4iN7j3xcbZlFIvIbooPi'
+end
