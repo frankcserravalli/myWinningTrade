2b9d15a84913c76032c127fc285349b7f6faf642
Jason <jasontanner328@gmail.com>
2013-03-11 22:34:57 -0400

    added new code to try and put cron details into logger

--- lib/clock.rb
+++ lib/clock.rb
@@ -9,12 +9,12 @@ handler do |job|
 end
 
 every(60.seconds, 'evaluating stop loss orders...'){
-  #Delayed::Job.enqueue
+  puts "cron_check stop_loss_t"
+  Rails.logger.info "cron_check date time working, Log Date: #{DateTime.now}"
   StopLossTransaction.evaluate_pending_orders
-  Rails.logger.add("cron_check date time working, Log Date: #{DateTime.now}")
 }
 every(60.seconds, 'evaluating date time orders...'){
-  #Delayed::Job.enqueue
+  puts "cron_check stop_loss_t"
+  Rails.logger.info "cron_check date time working, Log Date: #{DateTime.now}"
   DateTimeTransaction.evaluate_pending_orders
-  Rails.logger.add("cron_check date time working, Log Date: #{DateTime.now}")
 }
