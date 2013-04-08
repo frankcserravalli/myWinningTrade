ca7968ba9f7858b25028bcd7013ae7ee2eb57e06
Jason <jasontanner328@gmail.com>
2013-04-08 14:01:48 -0400

    began working on an autocomplete search for students

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,6 +1,8 @@
 $(function() {
+  // When an user types in a letter in the student name input field
   $(".new_group").on("keyup", "#search", function(){
-
+    // Here we are submitting the form if the user has inserted more than 3 characters
+    // into the student name input field.
     $(this).parents(".student-search-form").submit();
   });
 });
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,9 +18,17 @@ class GroupsController < ApplicationController
 
   # This action is used for our ajax call to return a list of users matching a name
   def search_students
-    unless params[:search].blank?
-      @group_users = User.where('name LIKE ?', "%#{params[:search]}%")#.paginate(:per_page => 5, :page => params[:page])
-    end
+      unless params[:search].blank?
+        @group_users = []
+
+        # TODO remember to add where clause only selecting students
+        group_users = User.where('name LIKE ?', "%#{params[:search].capitalize}%")#.paginate(:per_page => 5, :page => params[:page])
+
+        group_users.each do |user|
+          @group_users.push user.name
+        end
+
+      end
 
     respond_to do |format|
       format.js
@@ -33,6 +41,7 @@ class GroupsController < ApplicationController
 
   private
 
+  # This method is used to redirect users who are not signed in
   def redirect_not_signed_in_user
     unless teacher_signed_in?
       redirect_to teacher_sign_in_path
