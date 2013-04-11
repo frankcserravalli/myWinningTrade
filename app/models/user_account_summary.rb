71b46784dc5cf647190a44debc9befdc68338567
Jason <jasontanner328@gmail.com>
2013-04-11 14:58:51 -0400

    updated queries for finding users in leaderboard

--- app/models/user_account_summary.rb
+++ app/models/user_account_summary.rb
@@ -4,12 +4,14 @@ class UserAccountSummary < ActiveRecord::Base
   belongs_to :user
 
   def self.find_top_results(user_id)
-    @world_leader_board = UserAccountSummary.order("capital_total DESC").includes(:users).limit(20)
+    @world_leader_board = UserAccountSummary.includes(:user).order("capital_total DESC").limit(10)
 
     @class = GroupUser.find_by_user_id(user_id)
 
     if @class
-      @class_leader_board = GroupUser.where(group_id: @class.group_id).includes(:user)
+      @class_mates = GroupUser.where(group_id: @class.group_id).select("user_id")
+
+      @class_leader_board = UserAccountSummary.where(user_id: @class_mates).includes(:user).order("capital_total DESC").limit(10)
     else
       @class_leader_board = nil
     end
