c4f797ac2f3c4e17a2117ddcad7147db64c25292
Jason <jasontanner328@gmail.com>
2013-05-08 13:52:02 -0400

    removed more queries

--- app/models/user_account_summary.rb
+++ app/models/user_account_summary.rb
@@ -4,14 +4,14 @@ class UserAccountSummary < ActiveRecord::Base
   belongs_to :user
 
   def self.find_top_results(user_id)
-    world_leader_board = UserAccountSummary.includes(:user).order("capital_total DESC").limit(10)
+    world_leader_board = UserAccountSummary.order("capital_total DESC").limit(10)
 
     the_class = GroupUser.find_by_user_id(user_id)
 
     if the_class
       the_class_mates = GroupUser.where(group_id: @class.group_id).select("user_id")
 
-      class_leader_board = UserAccountSummary.where(user_id: the_class_mates).includes(:user).order("capital_total DESC").limit(10)
+      class_leader_board = UserAccountSummary.where(user_id: the_class_mates).order("capital_total DESC").limit(10)
     else
       class_leader_board = nil
     end
