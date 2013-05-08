59a62e6ed041b6e02841a50e34586d0810885c22
Jason <jasontanner328@gmail.com>
2013-05-08 17:40:11 -0400

    added logger stuff for params

--- app/controllers/api/v1/buys_controller.rb
+++ app/controllers/api/v1/buys_controller.rb
@@ -14,6 +14,8 @@ module Api
           # and save the iOS side from sending params with hashes within hashes
           params[:buy][:volume] = params[:volume]
 
+          puts params
+          
           @buy_order = Buy.new(params[:buy].merge(user: @user))
 
           if @buy_order.place!(@stock_details)
