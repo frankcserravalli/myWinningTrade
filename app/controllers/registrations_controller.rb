e090f87a91a0656a0a8314d5aed229eec6e50134
Waasi <waasi@Baymax.local>
2015-08-03 14:13:08 -0400

    Fixed a bug on the authentication and the guest user.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,18 @@
 # Overwriting devise registrations controller
 class RegistrationsController < Devise::RegistrationsController
+
+  def create
+    @user = signed_user
+    if @user.guest
+      @user.update_attributes(sign_up_params)
+      @user.guest = false
+      @user.save
+      sign_in_and_redirect @user
+    else
+      super
+    end
+  end
+
   private
 
   def sign_up_params
