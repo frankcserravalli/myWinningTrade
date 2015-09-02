1aab3ab039a4b97500ca708b9ba04810e6ac9360
Waasi <waasi@Baymax.local>
2015-09-02 18:16:25 -0400

    Fixed all specs there are 24 pending to be implemented and 2 commented examples that need to be reviewed

--- spec/controllers/sells_controller_spec.rb
+++ spec/controllers/sells_controller_spec.rb
@@ -10,7 +10,7 @@ describe SellsController do
   it 'should create a sell for a user' do
     @apple = Stock.where(symbol: 'AAPL').first_or_create!(name: 'Apple')
     VCR.use_cassette('quote') do
-      @stock_details = Finance.current_stock_details('AAPL')
+      @stock_details = Finance.stock_details_for_symbol('AAPL')
     end
     @buy = Buy.new(user: @user, volume: 20)
     @buy.place!(@stock_details)
--- spec/models/finance_spec.rb
+++ spec/models/finance_spec.rb
@@ -7,27 +7,36 @@ describe "Finance" do
 
 	it "should return the details of a given stock" do
 		VCR.use_cassette('quote') do
-			quote = @api.current_stock_details('AAPL')
+			quote = @api.stock_details_for_symbol('AAPL')
 
 			quote.symbol.should == 'AAPL'
-			quote.name.should == 'Apple Inc.'
+			quote.Name.should == 'Apple Inc.'
 
-			[:name, :symbol, :ask, :ask_realtime, :days_range, :year_range, :open, :previous_close, :volume, :dividend_yield, :earnings_share, :stock_exchange, :last_trade_time, :current_price, :current_bid, :eps_estimate_current_year, :eps_estimate_next_year, :eps_estimate_next_quarter, :pe_ratio, :two_hundred_day_moving_average, :fifty_day_moving_average].each do |key|
+			[:Name, :Symbol, :Ask, :Open, :PreviousClose, :Volume].each do |key|
 				quote.respond_to?(key).should be_true
 			end
 		end
 	end
 
-  it "should return the details of a list of stocks" do
+  it "should return the nil if a stock symbol in the list is invalid" do
     VCR.use_cassette('multiple_quotes') do
       quote = @api.stock_details_for_list(['AAPL', 'GOOG', 'fake'])
-      quote.keys.should == ['AAPL', 'GOOG', 'fake']
-      quote['AAPL'].symbol.should == 'AAPL'
-      quote['fake'].should be_nil
+      quote.to_h.keys.should == [:AAPL, :GOOG]
+      quote[:fake].should be_nil
     end
   end
 
-  xit "should return the price of a stock every week day for the past 6 months" do
+  it "should return the details of a list of stocks" do
+    VCR.use_cassette('multiple_quotes') do
+      quote = @api.stock_details_for_list(['AAPL', 'GOOG'])
+      quote.to_h.keys.should == [:AAPL, :GOOG]
+      quote['AAPL'].symbol.should == 'AAPL'
+    end
+  end
+
+
+  # This needs to be refactored both the code and spec
+   pending it "should return the price of a stock every week day for the past 6 months" do
     cassette_date = Time.at(1348152000).to_date
 
     VCR.use_cassette('stock_price_history', :record => :new_episodes) do
